70cb28e960d5b154b2ddb95985c576bb7b1a3e81
PoggerPaul <114891770+21-PaulChong@users.noreply.github.com>
2022-11-01 10:34:23 +0800

    Update 5
    Testing that Destroying Asteroids Raises the Score

--- Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
+++ Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
@@ -82,6 +82,18 @@ public class TestSuite
         yield return null;
     }
 
+    [UnityTest]
+    public IEnumerator DestroyedAsteroidRaisesScore()
+    {
+        // 1
+        GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+        asteroid.transform.position = Vector3.zero;
+        GameObject laser = game.GetShip().SpawnLaser();
+        laser.transform.position = Vector3.zero;
+        yield return new WaitForSeconds(0.1f);
+        // 2
+        Assert.AreEqual(game.score, 1);
+    }
 
 
 }
