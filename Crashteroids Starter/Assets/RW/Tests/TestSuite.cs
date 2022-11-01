91af7572a7d0f311db6bb125365ef5080041f039
PoggerPaul <114891770+21-PaulChong@users.noreply.github.com>
2022-11-01 10:09:22 +0800

    Update 3
    Using Integration Tests
    Setting Up and Tearing Down Phases

--- Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
+++ Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
@@ -8,26 +8,43 @@ using System.Collections;
 public class TestSuite
 {
     private Game game;
+    [SetUp]
+    public void Setup()
+    {
+        GameObject gameGameObject =
+            MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+        game = gameGameObject.GetComponent<Game>();
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        Object.Destroy(game.gameObject);
+    }
+
 
     // 1
     [UnityTest]
     public IEnumerator AsteroidsMoveDown()
     {
-        // 2
-        GameObject gameGameObject =
-            MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
-        game = gameGameObject.GetComponent<Game>();
-        // 3
         GameObject asteroid = game.GetSpawner().SpawnAsteroid();
-        // 4
         float initialYPos = asteroid.transform.position.y;
-        // 5
         yield return new WaitForSeconds(0.1f);
-        // 6
+ 
         Assert.Less(asteroid.transform.position.y, initialYPos);
-        // 7
-        Object.Destroy(game.gameObject);
     }
+
+    [UnityTest]
+    public IEnumerator GameOverOccursOnAsteroidCollision()
+    {
+
+        GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+        asteroid.transform.position = game.GetShip().transform.position;
+        yield return new WaitForSeconds(0.1f);
+
+        Assert.True(game.isGameOver);
+    }
+
 
 }
 
